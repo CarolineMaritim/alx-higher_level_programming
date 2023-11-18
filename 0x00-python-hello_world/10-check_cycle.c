a5a34ae06221b106e6b8b29149359df3281a7964
CarolineMaritim <carolynamaritim@gmail.com>
2023-11-18 15:08:20 +0300

    interview prep

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,18 +12,19 @@ int check_cycle(listint_t *list)
 	listint_t *head, *link;
 	/*check for errors*/
 	if (list == NULL || list->next == NULL)
+	{
 		return (0);
+	}
 
-	head = list->next;
-	link = list->next->next;
+	head = list;
+	link = head->next;
 
-	while (head && link && link->next)
+	while (head != NULL && (link->next != NULL) && (head->next->next != NULL))
 	{
 		if (head == link)
-			return (1);/*check for cycle*/
-
-		head = link->next;
-		link = link->next->next;/*preserve the link*/
+			return (1);
+		head = head->next;
+		link = link->next->next;
 	}
 
 	return (0);
