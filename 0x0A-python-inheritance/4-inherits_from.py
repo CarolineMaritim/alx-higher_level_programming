3cf1fe4711e689048f6479ce6c0c020472932466
Caroline Chepkoech <99406341+CarolineMaritim@users.noreply.github.com>
2023-12-05 08:57:25 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,8 @@ instance of a class."""
 
 
 def inherits_from(obj, a_class):
+    """func that checks if obj is an inheritance
+    of a class"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
