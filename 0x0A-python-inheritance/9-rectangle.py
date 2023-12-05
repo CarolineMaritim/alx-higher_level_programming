c8f6a5c1df233077974019213208ab9fe5c051c2
Caroline Chepkoech <99406341+CarolineMaritim@users.noreply.github.com>
2023-12-05 09:05:41 +0300

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,17 +4,21 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Child class Rectangle"""
 
     def __init__(self, width, height):
+        """Defines a rectangle and checks it's dimensions"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
+        """Returns area of rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
+        """Returns a string defined by the rectangle"""
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
         return string
