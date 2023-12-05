f20f1f4c205ddada167e7ad66e842bf778e935be
Caroline Chepkoech <99406341+CarolineMaritim@users.noreply.github.com>
2023-12-05 08:51:47 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,12 @@
 
 
 class MyInt(int):
+    """Parent Class myInt"""
 
     def __eq__(self, value):
+        """returns true if equal"""
         return self.real != value
 
     def __ne__(self, value):
+        """Returns False if not equal"""
         return self.real == value
